27098f1e10ede60c36fee298c4ab0012a0d5baae
duaa-braik <duaa13braik@gmail.com>
2025-09-13 23:52:41 +0300

    Add a new sql query to get user by username

--- AccountManagement/DB.py
+++ AccountManagement/DB.py
@@ -4,6 +4,7 @@ def getDbConnection() -> sqlite3.Connection:
     connection = None
     try:
         connection = sqlite3.connect("Customers.db")
+        connection.row_factory = sqlite3.Row
     except:
         print("Failed connecting to the DB")
 
--- AccountManagement/customerDb.py
+++ AccountManagement/customerDb.py
@@ -4,3 +4,8 @@ def createCustomer(customer, userId, connection: Connection):
     cursor = connection.cursor()
     cursor.execute("insert into Customer values (?, ?, ?, ?, ?)", 
                    (userId, customer['firstName'], customer['lastName'], customer['password'], customer['username']))
+    
+def getCustomerByUsername(username, connection: Connection):
+    cusror = connection.execute("select password from Customer where username=?", (username,))
+    data = cusror.fetchone()
+    return dict(data) if data else None
