08c194b6d4f82171067cb3b26f5471d8fb64c59d
duaa-braik <duaa13braik@gmail.com>
2025-09-20 00:35:43 +0300

    Add a function to create DB tables if they don't exist

--- CustomersDB/DB.py
+++ CustomersDB/DB.py
@@ -9,3 +9,37 @@ def getDbConnection() -> sqlite3.Connection:
         print("Failed connecting to the DB")
 
     return connection
+
+def createTablesIfNotExist():
+    connection = getDbConnection()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Customer (
+            "Id"	TEXT,
+            "firstName"	TEXT,
+            "lastName"	TEXT,
+            "password"	TEXT,
+            "username"	TEXT,
+            PRIMARY KEY("Id")
+        )""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS `Job` (
+            "id"	TEXT,
+            "clientId"	TEXT,
+            "status"	TEXT,
+            PRIMARY KEY("id")
+        )
+    """)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS `Order` (
+            "id"	TEXT,
+            "status"	TEXT,
+            "customerId"	TEXT,
+            "total"	NUMERIC,
+            PRIMARY KEY("id")
+        );
+    """)
+    
+    connection.commit()
+    connection.close()
